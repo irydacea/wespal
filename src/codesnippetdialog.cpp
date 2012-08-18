c466d7ac4a4c3c517f86017c24142503bab568c9
Ignacio R. Morelle <shadowm@wesnoth.org>
2012-08-18 14:49:27 -0400

    codesnippetdialog: Hint at the clipboard contents by automatically selecting the whole code snippet each time the Copy option is used

--- src/codesnippetdialog.cpp
+++ src/codesnippetdialog.cpp
@@ -77,6 +77,7 @@ void CodeSnippetDialog::handleCopy()
 {
 	QApplication::clipboard()->setText(ui->teContents->toPlainText());
 	ui->boxClipboardMessage->setVisible(true);
+	ui->teContents->selectAll();
 }
 
 void CodeSnippetDialog::handleSave()
