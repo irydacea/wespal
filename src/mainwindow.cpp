2dafa036c89b0b6b883f7f174835d56fccc292dd
Iris Morelle <shadowm@wesnoth.org>
2019-03-27 18:16:07 -0300

    Use nullptr instead of 0 for null pointer values

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -95,10 +95,10 @@ MainWindow::MainWindow(QWidget *parent) :
 	save->setWhatsThis(tr("Saves the current recolor job."));
 	close->setWhatsThis(tr("Abandons the current job and exits."));
 
-	ui->action_Open->setIcon(this->style()->standardIcon(QStyle::SP_DialogOpenButton, 0, dynamic_cast<QWidget*>(ui->action_Open)));
-	ui->action_Save->setIcon(this->style()->standardIcon(QStyle::SP_DialogSaveButton, 0, dynamic_cast<QWidget*>(ui->action_Save)));
-	ui->action_Reload->setIcon(this->style()->standardIcon(QStyle::SP_BrowserReload, 0, dynamic_cast<QWidget*>(ui->action_Reload)));
-	ui->action_Quit->setIcon(this->style()->standardIcon(QStyle::SP_DialogCloseButton, 0, dynamic_cast<QWidget*>(ui->action_Quit)));
+	ui->action_Open->setIcon(this->style()->standardIcon(QStyle::SP_DialogOpenButton, nullptr, dynamic_cast<QWidget*>(ui->action_Open)));
+	ui->action_Save->setIcon(this->style()->standardIcon(QStyle::SP_DialogSaveButton, nullptr, dynamic_cast<QWidget*>(ui->action_Save)));
+	ui->action_Reload->setIcon(this->style()->standardIcon(QStyle::SP_BrowserReload, nullptr, dynamic_cast<QWidget*>(ui->action_Reload)));
+	ui->action_Quit->setIcon(this->style()->standardIcon(QStyle::SP_DialogCloseButton, nullptr, dynamic_cast<QWidget*>(ui->action_Quit)));
 
 	// Use theme icons for some buttons on X11. This is not
 	// in the .ui file for Qt 4.6 - 4.7 compatibility.
