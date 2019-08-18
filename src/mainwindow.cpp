2063d8ddd54076d9780f32f2cefe539657dcfbf9
Iris Morelle <shadowm@wesnoth.org>
2019-08-17 20:20:04 -0400

    Fix filename patterns for universally supported image formats

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -557,8 +557,8 @@ QString MainWindow::supported_file_patterns() const
 	QString ret;
 
 	QMap<QString, QString> all_fmts = {
-		{ ".png", tr("PNG image") },
-		{ ".bmp", tr("Windows Bitmap") },
+		{ "*.png", tr("PNG image") },
+		{ "*.bmp", tr("Windows Bitmap") },
 	};
 
 	QMap<QString, QString> optional_fmts = {
