f94cbb477f996e34887ff923769e35487eab8998
Iris Morelle <iris@irydacea.me>
2024-03-27 00:06:10 -0300

    utils: Add script for deploying on Windows

---
+++ utils/dist-windows.cmd
@@ -0,0 +1,109 @@
+@echo off
+rem
+rem Builds and deploys Wespal for Windows
+rem
+
+setlocal
+set devenv=%1
+set version=%2
+
+if .%version%==. goto badversion
+
+set appdir=Wespal
+set appexe=wespal.exe
+set archive=Wespal-%version%.zip
+
+set devenvcmd=
+if .%devenv%==.vs    goto vs
+if .%devenv%==.mingw goto mingw
+goto baddevenv
+
+:vs
+rem Visual Studio 2022 host
+set qtprefix=C:\Qt\6.6.2\msvc2019_64
+set devenvcmd="C:\Program Files\Microsoft Visual Studio\2022\Community"\VC\Auxiliary\Build\vcvars64.bat
+goto common
+
+:mingw
+rem Mingw host
+set qtprefix=C:\Qt\6.6.2\mingw_64
+set PATH=C:\Qt\Tools\mingw1120_64\bin;%PATH%
+goto common
+
+:common
+set cmakepath=C:\Qt\Tools\CMake_64\bin
+set ninjapath=C:\Qt\Tools\Ninja
+set windeployqt=%qtprefix%\bin\windeployqt.exe
+
+set distdir=.\dist\windows
+set builddir=.\build-dist-windows
+
+rem
+rem Set up development environment
+rem
+
+if not .%devenvcmd%==. call %devenvcmd%
+set PATH=%ninjapath%;%cmakepath%;%qtprefix%\bin;%PATH%
+
+rem
+rem Create and change into build directory
+rem
+
+rmdir /S /Q %builddir% > NUL
+mkdir %builddir%
+cd %builddir%
+
+rem
+rem Configure and build the app
+rem
+
+cmake -S .. -B . -G Ninja -DCMAKE_BUILD_TYPE=Release
+ninja
+
+rem
+rem Use windeployqt to deploy DLLs
+rem
+
+mkdir dist-prep
+move %appexe% dist-prep
+cd dist-prep
+"%windeployqt%" %appexe%
+
+rem
+rem Move deployed files into dist directory
+rem
+
+cd ..\..
+mkdir %distdir% > NUL
+move %builddir%\dist-prep %distdir%\%appdir%
+
+rem
+rem Create distribution archive
+rem
+
+powershell -Command "Compress-Archive %distdir%\%appdir% %distdir%\%archive%"
+
+rem
+rem Cleanup and report
+rem
+
+rmdir /S /Q %builddir% > NUL
+goto end
+
+:end
+echo Saved deployment files under %distdir%\:
+echo   * %appdir%\
+echo   * %archive%
+goto :eof
+
+:baddevenv
+echo You need to specify a development environment in the command line:
+goto usage
+
+:badversion
+echo You need to specify a version number in the command line:
+goto usage
+
+:usage
+echo   dist-windows.cmd vs    (version number)
+echo   dist-windows.cmd mingw (version number)
